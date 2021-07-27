0c1acecdf1e9d03d77be5c3ff8cc68fc8e5d3e07
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2021-07-28 05:56:18 +0900

    test

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 require "numo/narray"
 require "fromfile"
 
-a = Numo::DFloat[1..256].reshape(16, 16)
-a.tofile("data.bin")
-a = Numo::DFloat.fromfile("data.bin").reshape(16, 16)
+# a = Numo::DFloat[1..256].reshape(16, 16)
+# a.tofile("data.bin")
+# a = Numo::DFloat.fromfile("data.bin").reshape(16, 16)
