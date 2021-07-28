320fd4b9576d995d87be4b14453fd485825c64ed
ひかり <39254183+Himeyama@users.noreply.github.com>
2021-07-28 23:53:54 +0900

    #issue-954941489

--- ext/fromfile/extconf.rb
+++ ext/fromfile/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-narray_dir = Gem.find_files("numo")[0]
+narray_dir = Gem.find_files("numo").map{|e| e.include?("narray") ? e : false}.select{|e| e}.to_a[0]
 dir_config('narray', narray_dir, narray_dir)
 have_library("narray")
 
